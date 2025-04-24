17799af37cf69adf9c34fc3b1087863b04118be3
gqueen001 <queen01gi@gmail.com>
2025-04-24 20:12:23 +0500

    update: styles folder added login design and imported in login file

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -2,6 +2,7 @@ import { invoke } from "@tauri-apps/api/core";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TLogin } from "@/types";
+import { login } from "@/styles";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -12,16 +13,6 @@ const Login = () => {
     password: "",
     dataBase: "",
   });
-  const login = {
-    wrapper:
-      "text-[#B9B9B9] border border-[#2C8F13] w-lg h-auto p-[50px] rounded-lg flex flex-col gap-[30px]",
-    input:
-      "focus:outline-none border border-[#637160] p-2 rounded-xs focus:bg-[#135E00]",
-    inputs: "grid grid-cols-[100px_auto] gap-10 items-center",
-    label: "text-base",
-    button:
-      "border border-[#303030] p-2 rounded-xs w-[200px] m-auto cursor-pointer hover:bg-[#135E00]",
-  };
 
   const open_db_conn = async () => {
     try {
