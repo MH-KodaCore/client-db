576a4c1b1ef49e2408a3b5ce5ee52190fc6b4946
gqueen001 <queen01gi@gmail.com>
2025-04-24 17:00:11 +0500

    update: login ui with tailwind css

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,11 +1,13 @@
 const Login = () => {
   const login = {
     wrapper:
-      "text-[#B9B9B9] border border-[#2C8F13] w-lg h-auto p-[50px] rounded-lg items-center",
-    input: "border border-[#637160] p-2 rounded-xs",
-    form: "grid grid-cols-[100px_auto] gap-10",
+      "text-[#B9B9B9] border border-[#2C8F13] w-lg h-auto p-[50px] rounded-lg flex flex-col gap-[30px]",
+    input:
+      "focus:outline-none border border-[#637160] p-2 rounded-xs focus:bg-[#135E00]",
+    form: "grid grid-cols-[100px_auto] gap-10 items-center",
     label: "text-base",
-    button: "border border-[#303030] p-2 rounded-xs w-[200px]",
+    button:
+      "border border-[#303030] p-2 rounded-xs w-[200px] m-auto cursor-pointer hover:bg-[#135E00]",
   };
 
   return (
